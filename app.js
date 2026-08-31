dd790be5fff04fc879f488ad13822d2f1f97a141
Kamol Xusanov <xusanovk39@gmail.com>
2026-08-31 08:22:04 +0500

    fix: app.js, ()

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173/"
+    origin: process.env.CLIENT_URL ||  "http://localhost:5173"
 }))
 app.use(express.json())
 DBconnect();
